82579cce87d445e39a76b26723e601c0dbdbe434
annguyen <annguyen020403@gmail.com>
2026-04-11 20:32:56 +0700

    bài 1748

---
+++ 1748. Sum of Unique Elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        vector<int> dem(101, 0); // vì nums[i] <= 100
+        for (int x : nums) {
+            dem[x]++;
+        }
+        
+        int tong = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (dem[i] == 1) tong += i;
+        }
+        
+        return tong;
+    }
+};
